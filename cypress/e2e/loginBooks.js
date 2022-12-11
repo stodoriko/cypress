798996e55bdca872438cdfe9656ef4685223c2c6
stodoriko <s.todorikos@gmail.com>
2022-12-11 13:59:58 +0300

    third cypress commit

--- cypress/e2e/loginBooks.js
+++ cypress/e2e/loginBooks.js
@@ -1,60 +1,83 @@
-const bookFirst = {
-    title: "Московский клуб",
-    description:
-        "Когда-то Москва была столицей великой страны, потом она стала Анклавом, одним из многих. Теперь ее тайны могут спасти прижатый к стенке мир. Мир, в котором властвует Цифра, нанотехнологии и генная инженерия позволяют добиваться невероятных результатов, а могущественные корпорации соперничают с одряхлевшими государствами. Мир, который отчаянно пытается найти дорогу в будущее. Мир в котором борьба за власть достигла апогея.",
-    author: "Вадим Панов",
-};
+const {generateName} = require("../fixtures/data");
+const {mail, password} = require("../fixtures/login_data.json");
 
-const bookSecond = {
-    title: "Лучший экипаж солнечной",
-    description:
-        "Им надоело стрелять, но это единственное, что они умеют делать хорошо. Рано или поздно команда «К бою!» раздастся снова…",
-    author: "Олег Дивов",
-};
+const bookDescription = "Вы можете прочитать хорошую книгу";
+const bookAuthors = "Хороший автор";
 
-const bookThird = {
-    title: "Психология влияния",
-    description:
-        "Какие факторы заставляют одного человека говорить другому «да»? И какие методы наиболее эффективны, если необходимо добиться чужого согласия?",
-    author: "Роберт Чалдини",
-};
+it("Should login successfuly", () => {
+    cy.contains('Books list');
+    cy.login(mail, password);
+    cy.contains('Добро пожаловать test@test.com').should("be.visible");
+});
 
-describe("Favorite book spec", () => {
-    beforeEach(() => {
-        cy.visit("/");
-        cy.login("test@test.com", "test");
-    });
+it("Should not login with empty email", () => {
+    cy.login(" ", password);
+    cy.get('#mail').then($el => $el[0].checkValidity()).should("be.false");
+});
 
-    // it("Valid login", () => {
-    //   cy.contains("test@test.com").should("be.visible");
-    //   cy.contains("Add new").should("have.class", "btn");
-    // });
+it("Should not login with empty password", () => {
+    cy.contains('Books list');
+    cy.contains('Log in').click();
+    cy.get('#mail').type(mail);
+    cy.contains('Submit').click();
+    cy.get('#pass').then($el => $el[0].checkValidity()).should("be.false");
+});
 
-    it("Should add new book", () => {
-        cy.addBook(bookFirst);
-        cy.get(".card-title").should("contain.text", bookFirst.title);
-    });
+it("Should create new book", () => {
+    const bookName = generateName(10);
+    cy.login(mail, password);
+    cy.createNewBook(bookName, bookDescription, bookAuthors);
+    // check that book exists in books list
+    cy.contains(bookName);
+});
 
-    it("Should add new book to favorite", () => {
-        cy.addFavoriteBook(bookSecond);
-        cy.visit("/favorites");
-        cy.get(".card-title").should("contain.text", bookSecond.title);
-    });
+it("Should add book in favorites during creation", () => {
+    const bookName = generateName(10);
+    cy.login(mail, password);
+    cy.addNewBookToFavoritesDuringCreation(bookName, bookDescription, bookAuthors);
+    // убеждаемся, что книга создана
+    cy.contains(bookName);
+    // переходим в избранное и убеждаемся, что данная книга там есть
+    cy.checkThatBookExistsInFavorites(bookName);
+});
 
-    it("Should add book to favorite through 'Book list' page", () => {
-        cy.addBookNoFavorite(bookFirst);
-        cy.contains(bookFirst.title)
-            .should("be.visible")
-            .within(() => cy.get(".card-footer > .btn").click({ force: true }));
-        cy.visit("/favorites");
-        cy.contains(bookFirst.title).should("be.visible");
-    });
+it("Should add new book to favorite in Books List Page ", () => {
+    const bookName = generateName(10);
+    cy.login(mail, password);
+    cy.createNewBook(bookName, bookDescription, bookAuthors);
+    cy.contains(bookName);
+    cy.addToFavorite(bookName);
+    // переходим в избранное и убеждаемся, что данная книга там есть
+    cy.checkThatBookExistsInFavorites(bookName);
+});
 
-    it("Should delete book from favorite", () => {
-        cy.visit("/favorites");
-        cy.contains(bookSecond.title)
-            .should("be.visible")
-            .within(() => cy.get(".card-footer > .btn").click({ force: true }));
-        cy.contains(bookSecond.title).should("not.exist");
-    });
+it("Should delete book from favorites in Favorites Page", () => {
+    const bookName = generateName(10);
+    cy.login(mail, password);
+    cy.createNewBook(bookName, bookDescription, bookAuthors);
+    cy.contains(bookName);
+    cy.addToFavorite(bookName);
+    // переходим в избранное и убеждаемся, что данная книга там есть
+    cy.checkThatBookExistsInFavorites(bookName);
+    // удаление книги из избранного в Избранных
+    cy.deleteBookFromFavorites(bookName, "Favorites");
+    // убеждаемся, что книги нет в избранных
+    cy.wait(3000);
+    cy.contains(`${bookName}`).should('not.exist');
+});
+
+it("Should delete book from favorites in Books list Page", () => {
+    const bookName = generateName(10);
+    cy.login(mail, password);
+    cy.createNewBook(bookName, bookDescription, bookAuthors);
+    cy.contains(bookName);
+    cy.addToFavorite(bookName);
+    // переходим в избранное и убеждаемся, что данная книга там есть
+    cy.checkThatBookExistsInFavorites(bookName);
+    // удаление книги из избранного в Books list
+    cy.deleteBookFromFavorites(bookName, "Books list");
+    // убеждаемся, что книги нет в избранных
+    cy.wait(3000);
+    cy.contains('Favorites').click();
+    cy.contains(`${bookName}`).should('not.exist');
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,50 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (email, password) => {
+    cy.contains('Log in').click();
+    cy.get('#mail').type(email);
+    cy.get('#pass').type(password);
+    cy.contains('Submit').click();
+});
+
+Cypress.Commands.add('createNewBook', (title, description, authors) => {
+    cy.contains('Add new').click();
+    cy.get('#title').type(title);
+    cy.get('#description').type(description);
+    cy.get('#authors').type(authors);
+    cy.contains('Submit').click();
+});
+
+Cypress.Commands.add('addNewBookToFavoritesDuringCreation', (title, description, authors) => {
+    cy.contains('Add new').click();
+    cy.get('#title').type(title);
+    cy.get('#description').type(description);
+    cy.get('#authors').type(authors);
+    cy.get('#favorite').click();
+    cy.contains('Submit').click();
+});
+
+Cypress.Commands.add('addToFavorite', (title) => {
+    cy.wait(3000);
+    // ищем родителя книги и добавляем в избранное
+    cy.xpath(`//div[./div[./div[contains(text(), "${title}")]]]`).within(() =>{
+        cy.contains('Add to favorite').click();
+    });
+});
+
+Cypress.Commands.add('checkThatBookExistsInFavorites', (title) => {
+    // ищем родителя книги и добавляем в избранное
+    cy.contains('Favorites').click();
+    cy.contains(`${title}`);
+});
+
+Cypress.Commands.add('deleteBookFromFavorites', (title, page) => {
+    if (page == "Favorites") {
+        cy.contains('Favorites').click();
+        cy.xpath(`//div[./div[./div[contains(text(), "${title}")]]]`).within(() => {
+            cy.contains('Delete from favorite').click()});
+    }
+    if (page == "Books list") {
+        cy.contains('Books list').click();
+        cy.xpath(`//div[./div[./div[contains(text(), "${title}")]]]`).within(() => {
+            cy.contains('Delete from favorite').click()});
+    }
+});
